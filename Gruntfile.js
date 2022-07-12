a34064e8229d6a4c2cf981dd19a5479c297c67c0
Toshil Tanwar (toshil.tanwar@dotsquares.com) <toshil.tanwar@dotsquares.com>
2022-07-12 13:52:06 +0000

    gruntfile update

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,10 +1,10 @@
-const { spawnSync } = require('child_process');
+const webpackConfig = require('./webpack-config');
+const { exec } = require("child_process");
+const jamboConfig = require('./jambo.json');
+
+const outputDir = jamboConfig.dirs.output;
 
 module.exports = function (grunt) {
-  const webpackConfig = require('./webpack-config');
-  const jamboConfig = require('./jambo.json');
-  
-  const outputDir = jamboConfig.dirs.output;
   grunt.initConfig({
     webpack: {
       myConfig: webpackConfig
@@ -12,17 +12,34 @@ module.exports = function (grunt) {
     watch: {
       all: {
         files: ['**', '!**/node_modules/**', `!${outputDir}/**`],
-        tasks: ['build-site'],
+        tasks: ['jambobuild', 'webpack',],
         options: {
           spawn: false,
         },
       },
     },
   });
+
   grunt.loadNpmTasks('grunt-webpack');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
-  grunt.registerTask('build-site', 'Builds the site', () => {
-    spawnSync('npm', ['run',  'build', '--silent'], { stdio: 'inherit' });
+  grunt.registerTask('jambobuild', 'Jambo build.',
+  function() {
+    // Force task into async mode and grab a handle to the "done" function.
+    var done = this.async();
+    // Run some sync stuff.
+    grunt.log.writeln('Processing task...');
+    // And some async stuff.
+    exec('npx jambo build', (error, stdout, stderr) => {
+      if (error) {
+        console.log(error.message);
+        done(false);
+        return;
+      }
+
+      stderr && console.error(stderr);
+      stdout && console.log(stdout);
+      done();
+    });
   });
 }
